0e0dbf7e36b58ff84480bc9cc0d23acdd60e9caf
uswitch-02 <chunxidubian64@gmail.com>
2023-07-27 13:23:51 +0000

    [start]java1

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample{
+  public static void mian(String[]args){
+    
+  }
+}
+// クラスの作成・javaを作成する単位
+// "Sample"はファイル名
+// {から｝までをブロック（クラスSampleの本体）と言ったりする。
+
+//   public static void mian(String[]args){}<=メインメソッド
+// プログラムが開始されるブロックのこと
